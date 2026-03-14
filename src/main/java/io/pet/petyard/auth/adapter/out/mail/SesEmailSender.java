486d630c455df1d9510ab8054876bf234972bba9
verlnn <mlnsukim2327@gmail.com>
2026-03-14 16:13:27 +0900

    feat: local SMTP/prod SES 이메일 발송 분리

---
+++ src/main/java/io/pet/petyard/auth/adapter/out/mail/SesEmailSender.java
@@ -0,0 +1,21 @@
+package io.pet.petyard.auth.adapter.out.mail;
+
+import io.pet.petyard.auth.application.port.out.EmailSender;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+
+@Component
+@Profile("prod")
+public class SesEmailSender implements EmailSender {
+
+    private static final Logger log = LoggerFactory.getLogger(SesEmailSender.class);
+
+    @Override
+    public void send(String to, String subject, String body) {
+        // TODO: AWS SES SDK 연동 예정
+        log.warn("[EMAIL][SES][TODO] SES sender not configured. to={}, subject={}", to, subject);
+    }
+}
--- src/main/java/io/pet/petyard/auth/adapter/out/mail/SmtpEmailSender.java
+++ src/main/java/io/pet/petyard/auth/adapter/out/mail/SmtpEmailSender.java
@@ -7,9 +7,11 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 @Component
+@Profile("local")
 @ConditionalOnBean(JavaMailSender.class)
 @ConditionalOnMissingBean(EmailSender.class)
 public class SmtpEmailSender implements EmailSender {
