c42e7e17a4bf08760578b15d367bb095ffc6ba95
verlnn <mlnsukim2327@gmail.com>
2026-03-19 20:21:31 +0900

    fix: health check log

--- src/main/java/io/pet/petyard/common/adapter/in/web/HealthController.java
+++ src/main/java/io/pet/petyard/common/adapter/in/web/HealthController.java
@@ -1,17 +1,20 @@
 package io.pet.petyard.common.adapter.in.web;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
 
+@Slf4j
 @RestController
 @RequestMapping("/api")
 public class HealthController {
 
     @GetMapping("/health")
     public Map<String, String> health() {
-        return Map.of("status", "ok");
+        log.info("=============== Health Check Success ===============");
+        return Map.of("status", "okkkkkk");
     }
 }
